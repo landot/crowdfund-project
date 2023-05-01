19a2aff70095d6b4976ab44e6ffbe84fa61d355f
landot <timowland@gmail.com>
2023-05-01 16:24:27 -0700

    chore: update todos

--- src/App.tsx
+++ src/App.tsx
@@ -20,17 +20,19 @@ function App() {
   /*
   Header (large screen) - left logo, 3 links on right (links underline when hovered)
   Header (small screen) - left logo, hamburger button containing 3 links on right when clicked
-  DONE: Button - base button (green with darker green hover)
-  DONE: Button - back this project (opens modal)
-  DONE: Button - Select Reward (opens modal)
-  DONE: Button - Bookmark button (shrinks)
-  DONE: Icon - Bookmark icon (exists in images)
-  DONE: Bar - tracks percentage of crowdfunding goal
-  DONE: Base Box Container - white with rounded edges
-  DONE: Reward Box - rounded gray border, pledge amount, number left, Select Reward button
-  DONE: Pop up modal - x button, some text, list of pledge options with radio buttons
-  DONE: Modal pledge option - radio button, pledge amount, number left, description. When clicked: allows you to enter a pledge amount with continue button
-  DONE: Modal thank you page - icon, thanks message, got it button
+  Minimum pledge validation in modal
+  Allow pre-selecting of modal items when select reward is clicked
+  Disable radio for out of stock pledges
+  Run Accessibility scanner on project
+  Allow tab-based navigation
+  Stop body scrolling when modal is open
+  Mobile Friendly:
+    - Header
+    - Content Header section
+    - Fundraising section
+    - about project section
+    - modal
+    - thank you modal
   */
 
   return (
